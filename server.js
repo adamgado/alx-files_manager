e2e25d2de97ab4ba92bfd382eb1fa737e414ad18
adamgado <adamgado33@gmail.com>
2024-12-11 14:33:56 -0800

    2

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 import express from 'express';
-import routes from './routes';
+import controllerRouting from './routes/index';
 
 const app = express();
-const PORT = process.env.PORT || 5000;
-
+const port = process.env.PORT || 5000;
 
 app.use(express.json());
+controllerRouting(app);
 
-app.use('/', routes);
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
+
+export default app;
