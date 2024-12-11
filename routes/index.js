fddccced2fa72ce8345d7273b718c710b24ea304
adamgado <adamgado33@gmail.com>
2024-12-11 13:47:07 -0800

    2

--- routes/index.js
+++ routes/index.js
@@ -1,61 +1,28 @@
-import express from 'express';
+import { Router } from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
 import FilesController from '../controllers/FilesController';
 
-const router = express.Router();
+const router = Router();
 
-const controller = (app) => {
-  app.use('/', router);
+router.get('/status', AppController.getStatus);
+router.get('/stats', AppController.getStats);
 
-  router.get('/status', (req, res) => {
-    AppController.getStatus(req, res);
-  });
+router.post('/users', UsersController.postNew);
 
-  router.get('/stats', (req, res) => {
-    AppController.getStats(req, res);
-  });
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
+router.get('/users/me', UsersController.getMe);
 
-  router.post('/users', (req, res) => {
-    UsersController.postNew(req, res);
-  });
+router.post('/files', FilesController.postUpload);
 
-  router.get('/connect', (req, res) => {
-    AuthController.getConnect(req, res);
-  });
+router.get('/files/:id', FilesController.getShow);
+router.get('/files', FilesController.getIndex);
 
-  router.get('/disconnect', (req, res) => {
-    AuthController.getDisconnect(req, res);
-  });
+router.put('/files/:id/publish', FilesController.putPublish);
+router.put('/files/:id/unpublish', FilesController.putUnpublish);
 
-  router.get('/users/me', (req, res) => {
-    UsersController.getMe(req, res);
-  });
+router.get('/files/:id/data', FilesController.getFile);
 
-  router.post('/files', (req, res) => {
-    FilesController.postUpload(req, res);
-  });
-
-  router.get('/files/:id', (req, res) => {
-    FilesController.getShow(req, res);
-  });
-
-  router.get('/files', (req, res) => {
-    FilesController.getIndex(req, res);
-  });
-
-  router.put('/files/:id/publish', (req, res) => {
-    FilesController.putPublish(req, res);
-  });
-
-  router.put('/files/:id/unpublish', (req, res) => {
-    FilesController.putUnpublish(req, res);
-  });
-
-  router.post('/files/:id/data', (req, res) => {
-    FilesController.getFile(req, res);
-  });
-};
-
-export default controller;
+export default router;
--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 import express from 'express';
-import controllerRouting from './routes/index';
+import routes from './routes';
 
 const app = express();
-const prt = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
+
 
 app.use(express.json());
-controllerRouting(app);
 
-app.listen(prt, () => {
-  console.log(`Server running on port ${prt}`);
+app.use('/', routes);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
-
-export default app;
