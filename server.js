34d0e399746b0068a2e9a4d8ba75f28318d041f2
adamgado <65188827+adamgado@users.noreply.github.com>
2024-12-12 01:26:16 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,9 +4,7 @@ import routes from './routes';
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// parse JSON request bodies
 app.use(express.json());
-// load routes
 app.use('/', routes);
 
 app.listen(PORT, () => {
